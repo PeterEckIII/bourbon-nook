333e6254f395c432056fd0f6b08711de09304fd8
Peter Eck <jpeckiii@gmail.com>
2023-03-28 13:19:28 -0500

    Add server-side sorting

--- app/components/Grids/TestGrid/TestGrid.tsx
+++ app/components/Grids/TestGrid/TestGrid.tsx
@@ -3,17 +3,36 @@ import type { ChangeEvent } from "react";
 import { useTypedFetcher } from "remix-typedjson";
 import Spinner from "~/components/Icons/Spinner";
 import type { GridBottle } from "~/models/bottle.server";
-import type { BottleSearchData } from "~/routes/services/search/bottle/fetch";
-import type { Column, Limit } from "./NewTestGrid";
+import type {
+  BottleSearchData,
+  Column,
+  Limit,
+} from "~/routes/services/search/bottle/fetch";
+import type {} from "./NewTestGrid";
 import useDebounce from "~/utils/useDebounce";
 import GlobalFilter from "../Common/GlobalFilter";
 import Pagination from "../Common/Pagination/Pagination";
 import Table from "../Common/Table";
+import FilterArrows from "~/components/Icons/FilterArrows";
+import FilterArrowUp from "~/components/Icons/FilterArrowUp";
+import FilterArrowDown from "~/components/Icons/FilterArrowDown";
+
+export type SortDirection = "asc" | "desc";
+export type SortFields =
+  | "name"
+  | "status"
+  | "type"
+  | "distiller"
+  | "producer"
+  | "country"
+  | "region";
 
 export default function Grid() {
   const [currentPage, setCurrentPage] = useState<number>(0);
   const [limit, setLimit] = useState<Limit>(10);
   const [query, setQuery] = useState<string>("");
+  const [sortField, setSortField] = useState<SortFields>("name");
+  const [sortDirection, setSortDirection] = useState<SortDirection>("asc");
   const searchTerm = useDebounce(query, 300);
 
   const columns: Column[] = useMemo(
@@ -21,58 +40,86 @@ export default function Grid() {
       {
         header: "Name",
         field: "name",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Status",
         field: "status",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Type",
         field: "type",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Distiller",
         field: "distiller",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Producer",
         field: "producer",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Price",
         field: "price",
+        sort: false,
+        sortDirection: "desc",
       },
       {
         header: "Barrel #",
         field: "batch",
+        sort: false,
+        sortDirection: "desc",
       },
       {
         header: "ABV",
         field: "alcoholPercent",
+        sort: false,
+        sortDirection: "desc",
       },
       {
         header: "Proof",
         field: "proof",
+        sort: false,
+        sortDirection: "desc",
       },
       {
         header: "Country",
         field: "country",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Region",
         field: "region",
+        sort: true,
+        sortDirection: "desc",
       },
       {
         header: "Color",
         field: "color",
+        sort: false,
+        sortDirection: "desc",
       },
       {
         header: "Finishing",
         field: "finishing",
+        sort: false,
+        sortDirection: "desc",
       },
       {
         header: "Size",
         field: "size",
+        sort: false,
+        sortDirection: "desc",
       },
     ],
     []
@@ -97,16 +144,23 @@ export default function Grid() {
   useEffect(() => {
     function loadPageData() {
       load(
-        `/services/search/bottle/fetch?query=${searchTerm}&page=${currentPage}&limit=${limit}`
+        `/services/search/bottle/fetch?query=${searchTerm}&page=${currentPage}&limit=${limit}&sort=${sortField}&direction=${sortDirection}`
       );
     }
     loadPageData();
-  }, [load, currentPage, limit, searchTerm]);
+  }, [load, currentPage, limit, searchTerm, sortField, sortDirection]);
 
   const onFirst = () => setCurrentPage(0);
   const onLast = () => setCurrentPage(totalPages - 1);
   const handleQueryChange = (e: ChangeEvent<HTMLInputElement>) =>
     setQuery(e.target.value);
+
+  const handleSortingChange = (field: SortFields) => {
+    const sortOrder =
+      field === sortField && sortDirection === "asc" ? "desc" : "asc";
+    setSortField(field);
+    setSortDirection(sortOrder);
+  };
 
   return (
     <div className="">
@@ -130,22 +184,49 @@ export default function Grid() {
               </caption>
               <thead className="border-b-2 border-blue-500">
                 <tr className="overflow-hidden border-b-2 text-gray-700">
-                  {columns.map((column, index) => (
-                    <th
-                      scope="col"
-                      key={column.field}
-                      className="border-b-2 text-left text-gray-700 first-of-type:sticky first-of-type:left-0 first-of-type:w-[300px] first-of-type:min-w-[300px] first-of-type:bg-white"
-                    >
-                      <div className="resize overflow-auto">
-                        {column.header}
-                      </div>
-                    </th>
-                  ))}
+                  {columns.map((column, index) => {
+                    return column.sort ? (
+                      <th
+                        scope="col"
+                        key={column.field}
+                        onClick={() =>
+                          handleSortingChange(column.field as SortFields)
+                        }
+                        className="border-b-2 text-left text-gray-700 first-of-type:sticky first-of-type:left-0 first-of-type:w-[300px] first-of-type:min-w-[300px] first-of-type:bg-white"
+                      >
+                        <div className="flex resize justify-between overflow-auto">
+                          <div>{column.header}</div>
+                          <div className="mr-4">
+                            {column.sort &&
+                            column.field === sortField &&
+                            sortDirection === "asc" ? (
+                              <FilterArrowUp />
+                            ) : column.field === sortField &&
+                              sortDirection === "desc" ? (
+                              <FilterArrowDown />
+                            ) : (
+                              <FilterArrows />
+                            )}
+                          </div>
+                        </div>
+                      </th>
+                    ) : (
+                      <th
+                        scope="col"
+                        key={column.field}
+                        className="border-b-2 text-left text-gray-700 first-of-type:sticky first-of-type:left-0 first-of-type:w-[300px] first-of-type:min-w-[300px] first-of-type:bg-white"
+                      >
+                        <div className="flex resize-x justify-between overflow-auto">
+                          <div>{column.header}</div>
+                        </div>
+                      </th>
+                    );
+                  })}
                 </tr>
               </thead>
               <tbody>
                 {items.map((bottle, index) => (
-                  <tr key={bottle.id} className="h-[50px] overflow-hidden">
+                  <tr key={bottle.id} className="mx-2 h-[50px] overflow-hidden">
                     <th
                       scope="row"
                       className="sticky left-0 z-50 w-[300px] min-w-[300px] bg-white p-4 text-left"
